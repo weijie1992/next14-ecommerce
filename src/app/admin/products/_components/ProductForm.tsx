0991e13a1a9f6c53cdccc90d06068468169e5493
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-05-09 22:21:11 +0800

    add error handling for add product page

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,15 +6,18 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { formatCurrency } from '@/lib/formatters';
 import { useState } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 import { addProduct } from '../../_actions/products';
 
 export function ProductForm() {
+  const [error, action] = useFormState(addProduct, {});
   const [priceInCents, setPriceInCents] = useState<number>();
   return (
-    <form className="space-y-8" action={addProduct}>
+    <form className="space-y-8" action={action}>
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
         <Input type="text" id="name" name="name" required />
+        {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
 
       <div className="space-y-2">
@@ -30,24 +33,41 @@ export function ProductForm() {
         <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
         </div>
+        {error.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
         <Textarea id="description" name="description" required />
+        {error.description && (
+          <div className="text-destructive">{error.description}</div>
+        )}
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
         <Input type="file" id="file" name="file" required />
+        {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
         <Input type="file" id="image" name="image" required />
+        {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
 
-      <Button type="submit">Save</Button>
+      <SubmitButton />
     </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? 'Saving' : 'Save'}
+    </Button>
   );
 }
