187fa2a4ff44e3461daa89415c1f00f0e4661f35
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-05-09 22:36:47 +0800

    add veritical option and rename Order to orders foreiger key

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,11 +1,21 @@
 import { Button } from '@/components/ui/button';
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import db from '@/db/db';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
+import { CheckCircle2, MoreVertical, XCircle } from 'lucide-react';
 import Link from 'next/link';
 import PageHeader from '../_components/PageHeader';
 
@@ -23,7 +33,22 @@ export default function AdminProductPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: 'asc' },
+  });
+
+  if (products.length === 0) {
+    return <p>No products found</p>;
+  }
+
   return (
     <Table>
       <TableHeader>
@@ -40,7 +65,48 @@ function ProductsTable() {
         </TableRow>
       </TableHeader>
 
-      <TableBody></TableBody>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <span className="sr-only">Available</span>
+                  <CheckCircle2 />
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Unavailable</span>
+                  <XCircle />
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem asChild>
+                    <a download href={`/admin/products/${product.id}/download`}>
+                      Download
+                    </a>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href={`/admin/products/${product.id}/edit`}>
+                      Edit
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   );
 }
