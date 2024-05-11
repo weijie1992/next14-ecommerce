c77dc74a3135390afeed70b346e0628f40f27ed1
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-05-12 00:23:16 +0800

    add customer homepage, most popular and newest

---
+++ src/app/(customerFacing)/layout.tsx
@@ -0,0 +1,21 @@
+import { Nav, NavLink } from '@/components/Nav';
+
+//force nextjs to not cache any pages
+export const dynamic = 'force-dynamic';
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/products">Products</NavLink>
+        <NavLink href="/orders">My Orders</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+}
